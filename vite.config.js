9a514f547b7b885f382b539389fc274ef2b1075d
martin <martinquehace@gmail.com>
2023-11-28 10:21:52 -0500

    docs nueva Url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jurnalApp-_-/',
   plugins: [react()],
+  base: '/jurnalApp-_-/',
+  build: {
+    outDir: './docs'
+  }
 })
